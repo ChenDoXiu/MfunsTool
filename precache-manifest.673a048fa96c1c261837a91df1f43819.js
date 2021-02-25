6c1c9d78d1dbaadd4a3905ad184f2a265c9e263a
ChenDoXiu <2839673621@qq.com>
2021-02-25 15:57:33 +0800

    deploy

---
+++ precache-manifest.673a048fa96c1c261837a91df1f43819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e1861c368f11bac53c",
+    "url": "css/chunk-vendors.363562cd.css"
+  },
+  {
+    "revision": "54021107fe32c1354db8e6ef85f27205",
+    "url": "img/logo.54021107.jpg"
+  },
+  {
+    "revision": "bb26eb3fa1411a1cb49a59a579528cd6",
+    "url": "index.html"
+  },
+  {
+    "revision": "ebeb24642e0b80ce6519",
+    "url": "js/app.0df56caf.js"
+  },
+  {
+    "revision": "e4e1861c368f11bac53c",
+    "url": "js/chunk-vendors.c9f5c1b0.js"
+  },
+  {
+    "revision": "9be2344fac8e9c23709e1417cde44e3d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
